a41ca1c7ad8e66dc94c07add8691a9063368e2d7
zeapiaji <zeapiaji@gmail.com>
2022-09-12 13:04:53 +0700

    All work except filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
 import Todo from "./components/Todo";
 
 function App(props) { 
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText= `${taskList.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = {id: `todo-${nanoid()}`, name, completed: false};
+    setTasks([...tasks, newTask]);
+   }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+   }
+
+  function deleteTask(id) { 
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+   }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
